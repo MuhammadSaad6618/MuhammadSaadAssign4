17fb0c685863ae2247f0f124ca8b2718fdbf8926
MuhammadSaad6618 <muhammadsaad.1274@gmail.com>
2022-04-10 14:47:31 -0400

    I have implemented the download fragment, my app successfully downloads images related to listview item selected and displays a progress Bar as well.

--- MuhammadAssign44/src/main/java/muhammad/saad/n01366618/DownloadFrag.java
+++ MuhammadAssign44/src/main/java/muhammad/saad/n01366618/DownloadFrag.java
@@ -1,5 +1,10 @@
 package muhammad.saad.n01366618;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +12,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -23,6 +41,16 @@ public class DownloadFrag extends Fragment {
 
     View view;
     ListView listView;
+    ImageView imageView;
+    ProgressBar progressBar;
+
+
+    // Progress dialog type (0 - for Horizontal progress bar)
+    public static final int progress_bar_type = 0;
+
+    // File url to download
+    private static String file_url = "https://api.androidhive.info/progressdialog/hive.jpg";
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -68,14 +96,93 @@ public class DownloadFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_download, container, false);
+        view = inflater.inflate(R.layout.fragment_download, container, false);
         listView = view.findViewById(R.id.muhammad_listView);
+        progressBar = view.findViewById(R.id.muhammad_progressBar);
+        imageView = view.findViewById(R.id.muhammad_my_image);
+        progressBar.setVisibility(view.INVISIBLE);
         List<String> list = new ArrayList<>();
         list.add("Pakistan Flag");
         list.add("Canada Flag");
         list.add("Australia Flag");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,list);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String n = list.get(i);
+                if (n == "Pakistan Flag") {
+                    // Toast.makeText(getContext(),"this is it",Toast.LENGTH_SHORT).show();
+                    new Downloader().execute("https://i.pinimg.com/originals/dd/b0/fa/ddb0fa82fd09bd1bbc3654b652836714.png");
+
+
+                } else if(n == "Canada Flag") {
+                    new Downloader().execute("https://cdn.imgbin.com/25/9/8/imgbin-flag-of-canada-maple-leaf-national-flag-canada-flag-zrp0KhJpzGJEPxR8zDqhTKxH4.jpg");
+                }
+
+                else if(n == "Australia Flag") {
+                    new Downloader().execute("https://cdn3.iconfinder.com/data/icons/world-flags-square-vol-1/48/Australia-512.png");
+                }
+
+            }
+        });
         return view;
     }
+
+
+    private class Downloader extends AsyncTask<String, Void, Bitmap> {
+        HttpURLConnection httpURLConnection;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                Thread.sleep(6000);
+                URL url = new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = new BufferedInputStream(httpURLConnection.getInputStream());
+                Bitmap temp = BitmapFactory.decodeStream(inputStream);
+                return temp;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                httpURLConnection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if(imageView!= null) {
+                progressBar.setVisibility(View.INVISIBLE);
+                imageView.setImageBitmap(bitmap);
+            }
+
+            else {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+    }
+
+
+
+
 }
