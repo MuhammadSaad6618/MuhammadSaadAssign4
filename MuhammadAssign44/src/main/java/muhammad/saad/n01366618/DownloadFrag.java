86fccff0ae8bad98b4dca75432c4424f811e576d
MuhammadSaad6618 <muhammadsaad.1274@gmail.com>
2022-04-11 19:04:52 -0400

    This is the final commit

--- MuhammadAssign44/src/main/java/muhammad/saad/n01366618/DownloadFrag.java
+++ MuhammadAssign44/src/main/java/muhammad/saad/n01366618/DownloadFrag.java
@@ -45,8 +45,7 @@ public class DownloadFrag extends Fragment {
     ListView listView;
     ImageView imageView;
     ProgressBar progressBar;
-
-
+    
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
